fb7dff7cd8c3eb6b247e08a5393037f39ccf8b1e
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-05-24 00:13:50 -0400

    Update 404 page with home link

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,30 @@
 'use client';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 const PageNotFound = () => {
   const pathname = usePathname();
 
   return (
-    <div className='flex flex-col w-full  max-w-prose h-[calc(100vh-200px)] justify-center items-center'>
-      <div className='flex flex-col p-6 items-center justify-center gap-2 rounded-xl bg-[#f6f8fa] w-full'>
-        <FontAwesomeIcon icon={faSearch} className='fa-3x mb-2' />
+    <div className='w-full h-[calc(100vh-200px)] flex justify-center items-center'>
+      <div className='flex flex-col w-full max-w-prose justify-center items-center bg-gray-100 text-center p-6 rounded-lg shadow-lg'>
+        <h1 className='text-4xl font-bold text-gray-800 mb-4'>
+          404 - Page Not Found
+        </h1>
+        <p className='text-lg text-gray-600 mb-8'>
+          The page you are looking for,{' '}
+          <span className='text-red-500'>{pathname}</span>, does not exist.
+        </p>
 
-        <div className='flex flex-col items-center'>
-          <h4 className='mt-3 text-xl'>Page not found</h4>
-          <span className='text-lg text-gray-400'>{pathname}</span>
-        </div>
+        <Link
+          href='/'
+          className='flex items-center text-blue-600 hover:text-blue-800'
+        >
+          <FontAwesomeIcon icon={faHome} className='mr-2' />
+          Go back to Home
+        </Link>
       </div>
     </div>
   );
