4f0dbecebe26d5e12681ee0bfc97f4e6ebb28b57
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-06-04 09:00:17 -0400

    update login

--- src/components/Login/degen/ConnectComponent.tsx
+++ src/components/Login/degen/ConnectComponent.tsx
@@ -5,7 +5,9 @@ import { OnProviderLoginType } from '@multiversx/sdk-dapp/types';
 import { usePathname } from 'next/navigation';
 
 const desktopLoginComponent = (
-  <span className='flex items-center gap-2 h-full'>[connect wallet]</span>
+  <span className='flex items-center gap-2 h-full relative top-[-25px] right-[-20px] '>
+    [connect wallet]
+  </span>
 );
 
 const ConnectComponent = () => {
@@ -19,7 +21,7 @@ const ConnectComponent = () => {
 
   return (
     <ExtensionLoginButton
-      className='!bg-transparent !border-0 hover:font-bold !p-0 !m-0'
+      className='!bg-transparent !border-0 hover:font-bold !p-0 !m-0 '
       {...commonProps}
     >
       {desktopLoginComponent}
