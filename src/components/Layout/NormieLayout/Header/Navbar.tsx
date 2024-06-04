d2d9902964bf2cddcc5daf4fa4fc42243d40f3bd
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-06-04 09:20:33 -0400

    update navbar

--- src/components/Layout/NormieLayout/Header/Navbar.tsx
+++ src/components/Layout/NormieLayout/Header/Navbar.tsx
@@ -1,79 +1,31 @@
 'use client';
-import { Button } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
-import { isPathOrSubpath } from '@/utils/urls';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
-  const pathname = usePathname();
+  // const pathname = usePathname();
 
   return (
-    <div className='text-sm sm:text-lg'>
-      <div className='flex gap-2 items-center '>
-        <Button
-          variant='ghost'
-          className={cn(
-            'text-gray-400 text-md lg:text-lg rounded-xl hover:bg-[#1c243e83] hover:text-white',
-            isPathOrSubpath(
-              pathname.split('/')[pathname.split('/').length - 1],
-              ['normie']
-            ) && ' text-white'
-          )}
-          asChild
-        >
-          <Link href='/'>
-            <span className='whitespace-nowrap'>Swap</span>
+    <div className='text-sm '>
+      <div>
+        <div className='flex gap-2 items-center'>
+          <Link href='/' className='hover:font-bold'>
+            [Swap]
           </Link>
-        </Button>
+          <Link href='/pools' className='hover:font-bold'>
+            [Pool]
+          </Link>
+          <div className='flex gap-2 items-center'>
+            <Link href='/dust' className='hover:font-bold'>
+              [Dust]
+            </Link>
+          </div>
+        </div>
 
-        <Button
-          variant='ghost'
-          className={cn(
-            'text-gray-400 text-md lg:text-lg rounded-xl hover:bg-[#1c243e83] hover:text-white',
-            isPathOrSubpath(
-              pathname.split('/')[pathname.split('/').length - 1],
-              ['pools']
-            ) && ' text-white'
-          )}
-          asChild
-        >
-          <Link href='/pools'>
-            <span className='whitespace-nowrap'>Pool</span>
+        <div className='flex gap-2 items-center'>
+          <Link href='#' className='hover:font-bold'>
+            [More]
           </Link>
-        </Button>
-
-        <Button
-          variant='ghost'
-          className={cn(
-            'text-gray-400 text-md lg:text-lg rounded-xl hover:bg-[#1c243e83] hover:text-white',
-            isPathOrSubpath(
-              pathname.split('/')[pathname.split('/').length - 1],
-              ['dust']
-            ) && ' text-white'
-          )}
-          asChild
-        >
-          <Link href='/dust'>
-            <span className='whitespace-nowrap'>Dust</span>
-          </Link>
-        </Button>
-
-        <Button
-          variant='ghost'
-          className={cn(
-            ' text-gray-400text-md lg:text-lg rounded-xl hover:bg-[#1c243e83] hover:text-white',
-            isPathOrSubpath(
-              pathname.split('/')[pathname.split('/').length - 1],
-              ['/']
-            ) && ' text-white'
-          )}
-          asChild
-        >
-          <Link href='#'>
-            <span className='whitespace-nowrap'>More</span>
-          </Link>
-        </Button>
+        </div>
       </div>
 
       <div className='flex gap-2 items-center'></div>
