6d17a81779a10e797d3a4df4aa95f0eb245813bc
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-06-11 11:03:00 -0400

    center go back button on dust

--- src/app/normie/views/DustView/DustView.tsx
+++ src/app/normie/views/DustView/DustView.tsx
@@ -4,12 +4,12 @@ import MoonDustXCard from './components/ConvertCard/MoonDustXCard';
 const DustView = () => {
   return (
     <>
-      <div className='flex justify-center items-center flex-col mt-5 w-full'>
-        <div className='w-4/5 max-w-[750px] mb-3'>
+      <div className='flex  items-center flex-col  w-full'>
+        <div className='w-4/5 max-w-[750px] mb-3 text-center'>
           <GoBackButton>[go back]</GoBackButton>
         </div>
 
-        <div className='max-w-[750px] w-4/5'>
+        <div className='w-full mt-5'>
           <MoonDustXCard />
         </div>
       </div>
