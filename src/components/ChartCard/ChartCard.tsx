0d9c54a352e29681812daf0b58032ed9a32810d4
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-06-20 13:07:23 -0400

    set trading view charts in ui

--- src/components/ChartCard/ChartCard.tsx
+++ src/components/ChartCard/ChartCard.tsx
@@ -1,29 +1,62 @@
 'use client';
 import { IPoolPair } from '@/app/normie/views/PoolsView/utils/types';
-import { useSwapContext } from '@/app/normie/views/SwapView/SwapContext';
-import { cn } from '@/lib/utils';
-import Iframe from 'react-iframe';
-import TokenInfo from './TokenInfo';
+import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+import {
+  ChartingLibraryWidgetOptions,
+  ResolutionString
+} from '../../../public/static/charting_library';
+const defaultWidgetProps: Partial<ChartingLibraryWidgetOptions> = {
+  symbol: 'AAPL',
+  interval: '1D' as ResolutionString,
+  library_path: 'static/charting_library/',
+  locale: 'en',
+  charts_storage_url: 'https://saveload.tradingview.com',
+  charts_storage_api_version: '1.1',
+  client_id: 'tradingview.com',
+  user_id: 'public_user_id',
+  fullscreen: false,
+  autosize: true
+};
+
+const TVChartContainer = dynamic(
+  () => import('./index').then((mod) => mod.TVChartContainer),
+  { ssr: false }
+);
 interface IProps {
   poolPair?: IPoolPair;
 }
-export default function ChartCard({ poolPair }: IProps) {
-  const swapCtx = useSwapContext();
+export default function ChartCard({}: IProps) {
+  const [isScriptReady, setIsScriptReady] = useState(false);
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '/static/datafeeds/udf/dist/bundle.js';
+    script.async = true;
+
+    script.onload = () => {
+      console.log('Is ready');
+      setIsScriptReady(true);
+    };
+
+    script.onerror = (e: ErrorEvent) => {
+      console.error('Script failed to load', e.error);
+    };
+
+    document.body.appendChild(script);
+
+    // Cleanup function to remove the script if the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []); // Empty dependency array means this effect runs once on mount and clean up on unmount
 
   return (
     <div
-      className={cn(
-        'h-full min-h-[450px]  rounded-sm  overflow-hidden md:block hidden',
-        !swapCtx.isOpenCharts && 'md:hidden'
-      )}
+      className={
+        'h-full min-h-[450px]  rounded-sm  overflow-hidden md:block hidden'
+      }
     >
-      <TokenInfo poolPair={poolPair} />
-      <Iframe
-        url='https://e-compass.io/embeddedChart/JEX-9040ca/USDC-c76f1f'
-        className='w-full min-h-[400px]'
-        display='block'
-        position='relative'
-      />
+      {isScriptReady && <TVChartContainer {...defaultWidgetProps} />}
     </div>
   );
 }
