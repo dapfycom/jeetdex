743dd47631b798c56dd8dc4e7b2c41f1a8444bbd
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-06-20 09:41:55 -0400

    add swr dev tools

--- src/providers/SwrProvider.tsx
+++ src/providers/SwrProvider.tsx
@@ -1,25 +1,28 @@
 'use client';
 import { PropsWithChildren } from 'react';
 import { SWRConfig } from 'swr';
+import { SWRDevTools } from 'swr-devtools';
 
 const SwrProvider = ({ children }: PropsWithChildren) => {
   return (
-    <SWRConfig
-      value={{
-        revalidateIfStale: false,
-        revalidateOnFocus: false,
-        onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
-          console.log('error', error);
-          console.log('key', key);
+    <SWRDevTools>
+      <SWRConfig
+        value={{
+          revalidateIfStale: false,
+          revalidateOnFocus: false,
+          onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+            console.log('error', error);
+            console.log('key', key);
 
-          if (error.status === 404) return;
-          if (retryCount >= 3) return;
-          setTimeout(() => revalidate({ retryCount }), 5000);
-        }
-      }}
-    >
-      {children}
-    </SWRConfig>
+            if (error.status === 404) return;
+            if (retryCount >= 3) return;
+            setTimeout(() => revalidate({ retryCount }), 5000);
+          }
+        }}
+      >
+        {children}
+      </SWRConfig>
+    </SWRDevTools>
   );
 };
 
