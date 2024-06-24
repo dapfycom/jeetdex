fac3feb8d97cc8b4f5daec23209474cc8b0e10bd
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-06-24 16:17:28 -0400

    hide issue a coin

--- src/components/CreatePoolModal/CreatePoolModal.tsx
+++ src/components/CreatePoolModal/CreatePoolModal.tsx
@@ -1,18 +1,11 @@
 'use client';
 import { Button, ButtonProps } from '@/components/ui/button';
 import useDisclosure from '@/hooks/useDisclosure';
-import {
-  faArrowLeft,
-  faCoins,
-  faWaterLadder
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import dynamic from 'next/dynamic';
 import { PropsWithChildren, createContext, useState } from 'react';
 import Modal from 'react-modal';
-import ActionsBox from './ActionsBox';
 
-const IssueToken = dynamic(() => import('@/components/IssueToken/IssueToken'));
+// const IssueToken = dynamic(() => import('@/components/IssueToken/IssueToken'));
 const CreatePoolsContainer = dynamic(
   () => import('../CreatePool/CreatePoolsContainer')
 );
@@ -27,7 +20,7 @@ const CreatePoolModal = ({
   ...props
 }: PropsWithChildren<ButtonProps>) => {
   const { isOpen, onToggle, onClose } = useDisclosure(false);
-  const [view, setView] = useState<'issue' | 'create-pool'>(null);
+  const [view, setView] = useState<'issue' | 'create-pool'>('create-pool');
   console.log('render');
 
   return (
@@ -43,7 +36,7 @@ const CreatePoolModal = ({
         contentLabel='Example Modal'
         className='fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200'
       >
-        {view === null && (
+        {/* {view === null && (
           <div className='grid grid-cols-2 gap-6 w-full mt-4'>
             <ActionsBox
               icon={faCoins}
@@ -56,8 +49,8 @@ const CreatePoolModal = ({
               onclick={() => setView('create-pool')}
             />
           </div>
-        )}
-        {view !== null && (
+        )} */}
+        {/* {view !== null && (
           <Button
             variant='ghost'
             className='w-fit hover:bg-[#47d4d413]'
@@ -66,10 +59,10 @@ const CreatePoolModal = ({
             <FontAwesomeIcon icon={faArrowLeft} className='mr-3' /> New coin
             today ?
           </Button>
-        )}
+        )} */}
 
         <viewContext.Provider value={{ view, setView }}>
-          {view === 'issue' && <IssueToken />}
+          {/* {view === 'issue' && <IssueToken />} */}
 
           {view === 'create-pool' && <CreatePoolsContainer />}
         </viewContext.Provider>
