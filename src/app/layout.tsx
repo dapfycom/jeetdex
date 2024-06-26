e6591494b0584c8c9264d10fb0f532cbca32c985
Armando Cesar Martin Calderon <armandoc9943@gmail.com>
2024-06-26 14:25:38 -0400

    update metas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,15 +11,42 @@ const fontSans = FontSans({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_FRONTED_URL),
   title: 'Jeet Dex',
   description:
     'Launch coins on MultiversX in just a few clicks: Get instant branding, real-time charts and chats.',
+  openGraph: {
+    title: 'Jeet Dex',
+    description:
+      'Launch coins on MultiversX in just a few clicks: Get instant branding, real-time charts and chats.',
+    url: './',
+    siteName: 'Jeet Dex',
+    images: ['/assets/img/social.png'],
+    locale: 'en_US',
+    type: 'website'
+  },
+  twitter: {
+    title: 'Jeet Dex',
+    card: 'summary_large_image',
+    images: ['/assets/img/social.png']
+  },
   viewport: {
     width: 'device-width',
     initialScale: 1
   },
   icons: {
     icon: '/favicon.ico'
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1
+    }
   }
 };
 
