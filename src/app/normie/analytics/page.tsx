4e863e8de8e7af3facbf94528ad64ce5878dff25
Armando C Martin <armandoc9943@gmail.com>
2024-07-11 13:03:45 -0400

    refactor (analitics): update layout

--- src/app/normie/analytics/page.tsx
+++ src/app/normie/analytics/page.tsx
@@ -86,134 +86,147 @@ export default function Analitics() {
 
   const isLoading = isLoadingjeet || isLoadingStatics || isLoadingTokens;
   return (
-    <div className='container mx-auto px-4 py-8 md:px-6 md:py-12'>
-      <h1 className='text-3xl font-bold mb-8'>Jeetdex Analytics</h1>
-
-      <section className='mb-12'>
-        <h2 className='text-2xl font-bold mb-4'>🔒Total Value Locked (TVL) </h2>
-        {!tvl ? (
-          <Skeleton className='w-[160px] h-[80px]' />
-        ) : (
-          <div className='bg-background rounded-lg shadow-lg p-6 w-fit'>
-            <div className='flex '>
-              <div className='text-center'>
-                <div className='text-2xl font-bold'>
-                  $ {formatNumber(tvl.toNumber())}
+    <div className='container mx-auto px-4 py-8 md:px-6 md:py-12 '>
+      <div className='flex flex-col sm:flex-row sm:gap-12 justify-center'>
+        <section className='mb-12'>
+          <h2 className='text-2xl font-bold mb-4'>
+            🔒Total Value Locked (TVL){' '}
+          </h2>
+          {!tvl ? (
+            <Skeleton className='w-[160px] h-[80px]' />
+          ) : (
+            <div className='bg-background rounded-lg shadow-lg p-6 w-full'>
+              <div className='flex '>
+                <div className='text-center'>
+                  <div className='text-2xl font-bold'>
+                    $ {formatNumber(tvl.toNumber())}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        )}
-      </section>
-      <section className='mb-12'>
-        <h2 className='text-2xl font-bold mb-4'>Daily Volume</h2>
-        {isLoading ? (
-          <Skeleton className='w-[160px] h-[80px]' />
-        ) : (
-          <div className='bg-background rounded-lg shadow-lg p-6 w-fit'>
-            <div className='flex '>
-              <div className='text-center'>
-                <div className='text-2xl font-bold'>
-                  ${' '}
-                  {formatBalanceDollar(
-                    {
-                      balance: data.volume,
-                      decimals: 0
-                    },
-                    elrondToken.price,
-                    true
-                  )}
+          )}
+        </section>
+        <section className='mb-12'>
+          <h2 className='text-2xl font-bold mb-4'>🌊 Daily Volume</h2>
+          {isLoading ? (
+            <Skeleton className='w-[160px] h-[80px]' />
+          ) : (
+            <div className='bg-background rounded-lg shadow-lg p-6 w-full'>
+              <div className='flex '>
+                <div className='text-center'>
+                  <div className='text-2xl font-bold'>
+                    ${' '}
+                    {formatBalanceDollar(
+                      {
+                        balance: data.volume,
+                        decimals: 0
+                      },
+                      elrondToken.price,
+                      true
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
+          )}
+        </section>
+      </div>
+      <div className='flex w-full flex-col items-center'>
+        <section className='mb-12'>
+          <h2 className='text-2xl font-bold mb-4'>💪 Top Gainers</h2>
+          <div className='flex  flex-wrap gap-4'>
+            {isLoading ? (
+              <>
+                {[1, 2, 3, 4, 5, 6].map((v) => {
+                  return (
+                    <Skeleton
+                      key={v}
+                      className=' w-full sm:w-[228px] h-[80px]'
+                    />
+                  );
+                })}
+              </>
+            ) : (
+              <>
+                {topGainers.map((coin) => (
+                  <div
+                    key={coin.symbol}
+                    className='bg-background rounded-lg shadow-lg p-4 w-full sm:w-[228px]'
+                  >
+                    <div className='flex items-center justify-between mb-2'>
+                      <div className='flex items-center gap-2'>
+                        <div className='bg-muted rounded-full w-8 h-8 flex items-center justify-center'>
+                          <span className='text-muted-foreground font-bold'>
+                            {coin.symbol}
+                          </span>
+                        </div>
+                        <span className='font-medium'>{coin.name}</span>
+                      </div>
+                      <span
+                        className={`font-medium ${
+                          coin.change > 0 ? 'text-green-500' : 'text-red-500'
+                        }`}
+                      >
+                        {coin.change.toFixed(2)}%
+                      </span>
+                    </div>
+                    <div className='text-2xl font-bold'>
+                      ${coin.price.toFixed(2)}
+                    </div>
+                  </div>
+                ))}
+              </>
+            )}
           </div>
-        )}
-      </section>
-
-      <section className='mb-12'>
-        <h2 className='text-2xl font-bold mb-4'>Top Gainers</h2>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4'>
-          {isLoading ? (
-            <>
-              {[1, 2, 3, 4, 5, 6].map((v) => {
-                return <Skeleton key={v} className='w-full h-[80px]' />;
-              })}
-            </>
-          ) : (
-            <>
-              {topGainers.map((coin) => (
-                <div
-                  key={coin.symbol}
-                  className='bg-background rounded-lg shadow-lg p-4'
-                >
-                  <div className='flex items-center justify-between mb-2'>
-                    <div className='flex items-center gap-2'>
-                      <div className='bg-muted rounded-full w-8 h-8 flex items-center justify-center'>
-                        <span className='text-muted-foreground font-bold'>
-                          {coin.symbol}
-                        </span>
+        </section>
+        <section className='mb-12'>
+          <h2 className='text-2xl font-bold mb-4'>👇 Top Losers</h2>
+          <div className='flex  flex-wrap gap-4'>
+            {isLoading ? (
+              <>
+                {[1, 2, 3, 4, 5, 6].map((v) => {
+                  return (
+                    <Skeleton
+                      key={v}
+                      className='w-full sm:w-[228px] h-[80px]'
+                    />
+                  );
+                })}
+              </>
+            ) : (
+              <>
+                {topLosers.map((coin) => (
+                  <div
+                    key={coin.symbol}
+                    className='bg-background rounded-lg shadow-lg p-4 w-full sm:w-[228px]'
+                  >
+                    <div className='flex items-center justify-between mb-2'>
+                      <div className='flex items-center gap-2'>
+                        <div className='bg-muted rounded-full w-8 h-8 flex items-center justify-center'>
+                          <span className='text-muted-foreground font-bold'>
+                            {coin.symbol}
+                          </span>
+                        </div>
+                        <span className='font-medium'>{coin.name}</span>
                       </div>
-                      <span className='font-medium'>{coin.name}</span>
+                      <span
+                        className={`font-medium ${
+                          coin.change > 0 ? 'text-green-500' : 'text-red-500'
+                        }`}
+                      >
+                        {coin.change.toFixed(2)}%
+                      </span>
                     </div>
-                    <span
-                      className={`font-medium ${
-                        coin.change > 0 ? 'text-green-500' : 'text-red-500'
-                      }`}
-                    >
-                      {coin.change.toFixed(2)}%
-                    </span>
-                  </div>
-                  <div className='text-2xl font-bold'>
-                    ${coin.price.toFixed(2)}
-                  </div>
-                </div>
-              ))}
-            </>
-          )}
-        </div>
-      </section>
-      <section className='mb-12'>
-        <h2 className='text-2xl font-bold mb-4'>Top Losers</h2>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4'>
-          {isLoading ? (
-            <>
-              {[1, 2, 3, 4, 5, 6].map((v) => {
-                return <Skeleton key={v} className='w-full h-[80px]' />;
-              })}
-            </>
-          ) : (
-            <>
-              {topLosers.map((coin) => (
-                <div
-                  key={coin.symbol}
-                  className='bg-background rounded-lg shadow-lg p-4'
-                >
-                  <div className='flex items-center justify-between mb-2'>
-                    <div className='flex items-center gap-2'>
-                      <div className='bg-muted rounded-full w-8 h-8 flex items-center justify-center'>
-                        <span className='text-muted-foreground font-bold'>
-                          {coin.symbol}
-                        </span>
-                      </div>
-                      <span className='font-medium'>{coin.name}</span>
+                    <div className='text-2xl font-bold'>
+                      ${coin.price.toFixed(2)}
                     </div>
-                    <span
-                      className={`font-medium ${
-                        coin.change > 0 ? 'text-green-500' : 'text-red-500'
-                      }`}
-                    >
-                      {coin.change.toFixed(2)}%
-                    </span>
                   </div>
-                  <div className='text-2xl font-bold'>
-                    ${coin.price.toFixed(2)}
-                  </div>
-                </div>
-              ))}
-            </>
-          )}
-        </div>
-      </section>
+                ))}
+              </>
+            )}
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
