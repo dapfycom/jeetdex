f3143a89134620922f1f947f0934ebfa83365361
Armando C Martin <armandoc9943@gmail.com>
2024-07-30 12:42:38 -0400

    refactor: After 3 seconds, if the user is not logged in, connect to the dapp

--- src/components/Login/normie/Login.tsx
+++ src/components/Login/normie/Login.tsx
@@ -4,12 +4,12 @@ import DisconnectComponent from './DisconnectComponent';
 
 import { createAuthTokenCookie } from '@/actions/cookies';
 import { createProfile } from '@/actions/user';
-import { useAppDispatch } from '@/hooks';
+import { useAppDispatch, useAuthentication } from '@/hooks';
 import useIsMobile from '@/hooks/useIsMobile';
 import { setShard, setUserAddress } from '@/redux/dapp/dapp-slice';
 import { useGetAccountInfo } from '@multiversx/sdk-dapp/hooks';
 import dynamic from 'next/dynamic';
-import { Suspense, useEffect } from 'react';
+import { Suspense, useEffect, useRef } from 'react';
 import { Skeleton } from '../../ui/skeleton';
 
 const ConnectComponent = dynamic(() => import('./ConnectComponent'), {
@@ -20,6 +20,8 @@ const Login = () => {
   const { isLoggedIn, tokenLogin } = useGetLoginInfo();
   const dispatch = useAppDispatch();
   const isMobile = useIsMobile(768);
+  const { handleConnect } = useAuthentication();
+  const hasTimeoutStarted = useRef(false);
 
   const { address, shard, account } = useGetAccountInfo();
   useEffect(() => {
@@ -34,6 +36,31 @@ const Login = () => {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [address]);
+
+  // After 3 seconds, if the user is not logged in, connect to the dapp
+  useEffect(() => {
+    if (!hasTimeoutStarted.current) {
+      hasTimeoutStarted.current = true;
+      localStorage.setItem('isLoggedIn2', 'false');
+      setTimeout(() => {
+        const isLoggedInLocalStorage = localStorage.getItem('isLoggedIn');
+        const isLoggedIn2LocalStorage = localStorage.getItem('isLoggedIn2');
+        if (
+          isLoggedInLocalStorage === 'false' &&
+          isLoggedIn2LocalStorage === 'false'
+        ) {
+          localStorage.setItem('isLoggedIn2', 'true');
+          handleConnect();
+        }
+      }, 3000);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('isLoggedIn', isLoggedIn.toString());
+  }, [isLoggedIn]);
 
   useEffect(() => {
     if (tokenLogin?.nativeAuthToken) {
