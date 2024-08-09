267045cd8b0f0650d2465815f055162d60e1190a
Armando C Martin <armandoc9943@gmail.com>
2024-08-09 12:02:35 -0400

    feat (pair page on degen): add trades to desktop

--- src/app/degen/pair/[address]/PairDesktop.tsx
+++ src/app/degen/pair/[address]/PairDesktop.tsx
@@ -1,10 +1,13 @@
 import DegenChart from '@/components/ChartCard/DegenChart';
 import GoBackButton from '@/components/GoBackButton';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { cn } from '@/lib/utils';
 import BondingProgress from './components/BondingProgress/BondingProgress';
 import DegenChats from './components/DegenChats/DegenChats';
 import DegenHolderList from './components/DegenHolderList/DegenHolderList';
 import DegenTokenSocials from './components/DegenTokenSocials/DegenTokenSocials';
+import DegenTrades from './components/DegenTrades/DegenTrades';
+import DegeUserTrades from './components/DegenTrades/DegeUserTrades';
 import ReachedCapBox from './components/ReachedCapBox/ReachedCapBox';
 import Swap from './components/Swap/Swap';
 
@@ -25,7 +28,26 @@ const PairDesktop = () => {
             <DegenChart />
           </div>
 
-          <DegenChats />
+          <Tabs
+            defaultValue='chats'
+            className={cn('w-full rounded-sm bg-[#1C243E] border-none  p-4')}
+          >
+            <TabsList className='w-full justify-start flex bg-transparent'>
+              <TabsTrigger value='chats'>chats</TabsTrigger>
+              <TabsTrigger value='trades'>trades</TabsTrigger>
+              <TabsTrigger value='my-trades'>my trades</TabsTrigger>
+            </TabsList>
+            <TabsContent value='chats'>
+              {' '}
+              <DegenChats />
+            </TabsContent>
+            <TabsContent value='trades'>
+              <DegenTrades />
+            </TabsContent>
+            <TabsContent value='my-trades'>
+              <DegeUserTrades />
+            </TabsContent>
+          </Tabs>
         </div>
 
         <div>
