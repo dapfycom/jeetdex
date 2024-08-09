f941d5d339d02593a989fa83573d1565dd40e522
Armando C Martin <armandoc9943@gmail.com>
2024-08-09 15:00:00 -0400

    refactor add some spacing to chats bottom

--- src/app/degen/pair/[address]/components/DegenChats/DegenChats.tsx
+++ src/app/degen/pair/[address]/components/DegenChats/DegenChats.tsx
@@ -6,7 +6,7 @@ const DegenChats = () => {
   const { coin } = useGetBoundingPair();
   if (!coin) return null;
   return (
-    <div className='w-full rounded-sm bg-[#1C243E] border-none  p-4'>
+    <div className='w-full rounded-sm bg-[#1C243E] border-none  p-4 mb-32'>
       <Chats poolPair={coin.firstTokenId} />
     </div>
   );
