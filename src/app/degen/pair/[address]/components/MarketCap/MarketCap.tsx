f2444303dbb62ff896661852bc9d8b971f974bac
Armando C Martin <armandoc9943@gmail.com>
2024-08-09 17:11:23 -0400

    update market cap

--- src/app/degen/pair/[address]/components/MarketCap/MarketCap.tsx
+++ src/app/degen/pair/[address]/components/MarketCap/MarketCap.tsx
@@ -1,31 +1,18 @@
 'use client';
-import useGetElrondToken from '@/hooks/useGetElrondToken';
-import { formatBalanceDollar } from '@/utils/mx-utils';
-import BigNumber from 'bignumber.js';
+import { formatBalance } from '@/utils/mx-utils';
 import { useGetBoundingPair } from '../../hooks';
 
 const MarketCap = () => {
   const { coin } = useGetBoundingPair();
-  const { elrondToken: firstToken } = useGetElrondToken(coin?.identifier);
-  const { elrondToken: secondToken } = useGetElrondToken(coin?.secondTokenId);
 
-  const firstTokenJeetdexPrice = new BigNumber(coin?.secondTokenReserve)
-    .dividedBy(coin?.firstTokenReserve)
-    .multipliedBy(secondToken?.price)
-    .toNumber();
-
-  if (!firstToken || !firstTokenJeetdexPrice || !coin) return null;
+  if (!coin) return null;
   return (
     <span className='text-primary text-left'>
       Market cap: $
-      {formatBalanceDollar(
-        {
-          balance: firstToken?.supply,
-          decimals: 0
-        },
-        firstTokenJeetdexPrice,
-        true
-      )}
+      {formatBalance({
+        balance: coin?.marketCap,
+        decimals: 6
+      })}
     </span>
   );
 };
