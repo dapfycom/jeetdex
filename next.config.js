ca07a26b6f5d521c1d6756b7d23b8e2b5283a8a4
Armando C Martin <armandoc9943@gmail.com>
2024-08-15 01:55:16 -0400

    refactor: remove server actions from config from next.config

--- next.config.js
+++ next.config.js
@@ -40,9 +40,6 @@ const nextConfig = {
     config.resolve.fallback = { fs: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
-  },
-  experimental: {
-    serverActions: true
   }
 };
 
